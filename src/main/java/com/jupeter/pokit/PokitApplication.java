fde634866f93232fec38161d8ff957e0b43d3d7d
7angJung <peterj0117@naver.com>
2026-04-24 03:39:55 +0900

    chore: initial project setup

---
+++ src/main/java/com/jupeter/pokit/PokitApplication.java
@@ -0,0 +1,13 @@
+package com.jupeter.pokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupeter/pokit/PokitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupeter.pokit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
